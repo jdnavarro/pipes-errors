2df3ae3e828711f889f4c93919f9e4fe8701d6af
Danny Navarro <j@dannynavarro.net>
2014-02-17 17:29:51 +0100

    Garden

--- Pipes/Lift/Either.hs
+++ Pipes/Lift/Either.hs
@@ -10,13 +10,17 @@ instance MFunctor (EitherT e) where
     hoist nat m = EitherT (nat (runEitherT m))
 
 -- | Wrap the base monad in 'EitherT'
-eitherP :: Monad m => Proxy a' a b' b m (Either e r) -> Proxy a' a b' b (EitherT e m) r
+eitherP :: Monad m
+        => Proxy a' a b' b m (Either e r)
+        -> Proxy a' a b' b (EitherT e m) r
 eitherP p = do
     x <- unsafeHoist lift p
     lift $ EitherT (return x)
 {-# INLINABLE eitherP #-}
 
 -- | Run 'EitherT' in the base monad
-runEitherP :: Monad m => Proxy a' a b' b (EitherT e m) r -> Proxy a' a b' b m (Either e r)
+runEitherP :: Monad m
+           => Proxy a' a b' b (EitherT e m) r
+           -> Proxy a' a b' b m (Either e r)
 runEitherP = runEitherT . distribute
 {-# INLINABLE runEitherP #-}
--- Pipes/Lift/EitherR.hs
+++ Pipes/Lift/EitherR.hs
@@ -18,14 +18,18 @@ instance MFunctor (EitherRT r) where
     hoist nat m = EitherRT (hoist nat (runEitherRT m))
 
 -- | Turn 'EitherT' in the base monad into 'EitherRT'
-eitherRP :: Monad m => Proxy a' a b' b (EitherT e m) r -> Proxy a' a b' b (EitherRT r m) e
+eitherRP :: Monad m
+         => Proxy a' a b' b (EitherT e m) r
+         -> Proxy a' a b' b (EitherRT r m) e
 eitherRP p = do
     x <- unsafeHoist lift $ runEitherP p
     lift . EitherRT . EitherT $ return x
 {-# INLINABLE eitherRP #-}
 
 -- | Turn 'EitherRT' in the base monad into 'EitherT'
-runEitherRP :: Monad m => Proxy a' a b' b (EitherRT r m) e -> Proxy a' a b' b (EitherT e m) r
+runEitherRP :: Monad m
+            => Proxy a' a b' b (EitherRT r m) e
+            -> Proxy a' a b' b (EitherT e m) r
 runEitherRP p = do
     x <- unsafeHoist lift . runEitherT . flipET . runEitherRT $ distribute p
     lift . EitherT . return $ flipE x
