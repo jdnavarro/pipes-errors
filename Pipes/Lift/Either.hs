7c947533b5c94c5835448cb70d9d9194565961c9
Danny Navarro <j@dannynavarro.net>
2014-02-17 10:27:05 +0100

    Documentation and silence orphan instance warning

--- Pipes/Lift/Either.hs
+++ Pipes/Lift/Either.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Pipes.Lift.Either where
 
 import Pipes (Proxy, MFunctor, hoist, lift)
@@ -8,12 +9,14 @@ import Control.Monad.Trans.Either (EitherT(..), runEitherT)
 instance MFunctor (EitherT e) where
     hoist nat m = EitherT (nat (runEitherT m))
 
+-- | Wrap the base monad in 'EitherT'
 eitherP :: Monad m => Proxy a' a b' b m (Either e r) -> Proxy a' a b' b (EitherT e m) r
 eitherP p = do
     x <- unsafeHoist lift p
     lift $ EitherT (return x)
 {-# INLINABLE eitherP #-}
 
+-- | Run 'EitherT' in the base monad
 runEitherP :: Monad m => Proxy a' a b' b (EitherT e m) r -> Proxy a' a b' b m (Either e r)
 runEitherP = runEitherT . distribute
 {-# INLINABLE runEitherP #-}
