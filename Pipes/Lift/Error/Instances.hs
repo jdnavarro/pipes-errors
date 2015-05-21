d95f0e1ca92afefbd20465cd5475a67a6fea6788
Danny Navarro <j@dannynavarro.net>
2015-05-21 09:07:36 +0200

    Remove duplicate imports

--- Pipes/Lift/Error/Instances.hs
+++ Pipes/Lift/Error/Instances.hs
@@ -2,7 +2,7 @@
 module Pipes.Lift.Error.Instances where
 
 import Pipes (MFunctor, hoist)
-import Control.Error (ExceptRT(..), runExceptT)
+import Control.Error (ExceptRT(ExceptRT, runExceptRT))
 
 instance MFunctor (ExceptRT r) where
     hoist nat m = ExceptRT (hoist nat (runExceptRT m))
